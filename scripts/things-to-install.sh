5357e0a1dc83d3665e8f24d2cd349dd401394873
fossephate <matt.cfosse@gmail.com>
2020-02-03 17:35:57 -0500

    update

--- scripts/things-to-install.sh
+++ scripts/things-to-install.sh
@@ -1,4 +1,4 @@
-sudo apt install nginx mongodb nodejs build-essential python3 python3-pip certbot
+sudo apt install nginx mongodb nodejs npm build-essential python3 python3-pip certbot redis
 
 sudo service nginx stop
 sudo certbot certonly
